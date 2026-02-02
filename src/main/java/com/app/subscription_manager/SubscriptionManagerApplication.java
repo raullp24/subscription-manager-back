1aed15b79d50c3f98deb82df6d415ccfaa67cf22
Lopez Pereiro <raul.lopez.pereiro@accenture.com>
2026-02-02 13:11:35 +0100

    first commit

---
+++ src/main/java/com/app/subscription_manager/SubscriptionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.app.subscription_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubscriptionManagerApplication.class, args);
+	}
+
+}
