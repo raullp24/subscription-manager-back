596255e2bcb7809adfe92f0ecd2ec1bd4ae2d64f
Lopez Pereiro <raul.lopez.pereiro@accenture.com>
2026-02-04 12:56:52 +0100

    Eliminado import inncesario

--- src/main/java/com/app/subscription_manager/config/SecurityConfiguration.java
+++ src/main/java/com/app/subscription_manager/config/SecurityConfiguration.java
@@ -12,8 +12,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import com.app.subscription_manager.service.CustomUsersDetailsService;
-
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
