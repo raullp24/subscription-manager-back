e0a0a5b7eb978a9dc8459218f013275f5142bbb7
raullp24 <raullopezpereiro@gmail.com>
2026-02-11 10:25:47 +0100

    Eliminado import innecesario

--- src/main/java/com/app/subscription_manager/web/SubscriptionController.java
+++ src/main/java/com/app/subscription_manager/web/SubscriptionController.java
@@ -2,7 +2,6 @@ package com.app.subscription_manager.web;
 
 import java.util.List;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
